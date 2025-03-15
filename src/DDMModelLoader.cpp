85170da4ba4ad312cf8be28ef1b8c06821989686
Daan Demaecker <75164311+DaanDemaecker@users.noreply.github.com>
2025-03-15 21:43:23 +0100

    Included glm, set up loadmodel function

--- src/DDMModelLoader.cpp
+++ src/DDMModelLoader.cpp
@@ -15,3 +15,19 @@ DDM::ModelLoader::~ModelLoader()
 {
 	std::cout << "ModelLoader destroyed \n";
 }
+
+void DDM::ModelLoader::LoadModel(const std::string& path, std::vector<Vertex>& vertices, std::vector<uint32_t>& indices)
+{
+	auto extension = GetExtension(path);
+
+	std::cout << "File extension: " << extension << " is not supported\n";
+
+}
+
+std::string DDM::ModelLoader::GetExtension(const std::string& filename)
+{
+	// Get the index of the final period in the name, all characters after it indicate the extension
+	auto index = filename.find_last_of(".");
+
+	return  filename.substr(index + 1, filename.size());
+}
--- src/DDMModelLoader.h
+++ src/DDMModelLoader.h
@@ -1,14 +1,29 @@
 // DDMModelLoader.h
 
 // This class will be the main object that will allow the user to load in models
-// It will serve
-
 
 #ifndef DDMModelLoaderIncluded
 #define DDMModelLoaderIncluded
 
+// File includes
+#include "glm/glm.hpp"
+
+// Standard library includes
+#include <string>
+#include <vector>
+
 namespace DDM
 {
+	struct Vertex
+	{
+		glm::vec3 pos;
+		glm::vec3 color;
+		glm::vec2 texCoordinates;
+		glm::vec3 normal;
+		glm::vec3 tangent;
+	};
+
+
 	class ModelLoader final
 	{
 	public:
@@ -25,9 +40,17 @@ namespace DDM
 		ModelLoader& operator=(ModelLoader& other) = delete;
 		ModelLoader& operator=(ModelLoader&& other) = delete;
 
+		// Load in a 3D model given a file path
+		// Parameters:
+		//     - path: The path to the model file
+		//     - vertices: The vector that will be used to store the vertices
+		//     - indices: The vector that will be used to store the indices
+		void LoadModel(const std::string& path, std::vector<Vertex>& vertices, std::vector<uint32_t>& indices);
+
+
 	private:
 
-
+		std::string GetExtension(const std::string& filename);
 	};
 }
 
