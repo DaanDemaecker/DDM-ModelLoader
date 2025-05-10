4effca5664ee0f18e6e97f8f5a3e85e73a79e81d
Daan Demaecker <75164311+DaanDemaecker@users.noreply.github.com>
2025-05-10 17:23:02 +0200

    Improved exception handling

--- DDMModelLoader/ModelLoaders/ModelLoader.cpp
+++ DDMModelLoader/ModelLoaders/ModelLoader.cpp
@@ -3,6 +3,9 @@
 // Header include
 #include "ModelLoader.h"
 
+// Standard library includes
+#include <stdexcept>
+
 DDMML::ModelLoader::ModelLoader(const std::string& extension)
 	:m_Extension{extension}
 {
@@ -15,10 +18,10 @@ DDMML::ModelLoader::ModelLoader(const std::string&& extension)
 
 void DDMML::ModelLoader::LoadModel(const std::string& fileName, Mesh* mesh)
 {
-	throw "Model loading is not implemented for ." + m_Extension + " files";
+	throw std::runtime_error("Model loading is not implemented for ." + m_Extension + " files");
 }
 
 void DDMML::ModelLoader::LoadScene(const std::string& fileName, std::vector<std::unique_ptr<Mesh>>& meshes)
 {
-	throw "Scene loading is not implemented for ." + m_Extension + " files";
+	throw std::runtime_error("Scene loading is not implemented for ." + m_Extension + " files");
 }
